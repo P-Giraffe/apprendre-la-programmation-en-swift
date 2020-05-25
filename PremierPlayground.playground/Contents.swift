45ddf615a8b24c5774c04c8a4c26d6fab7b0ff81
Maxime Britto <maxime@purplegiraffe.fr>
2020-05-25 19:14:38 +0200

    12_9_Utiliser_des_variables_en_Swift

--- PremierPlayground.playground/Contents.swift
+++ PremierPlayground.playground/Contents.swift
@@ -1,1 +1,6 @@
-print("Hello World!")
+let nomUtilisateur:String = "Sheldon"
+var score:Int = 0
+print("Hello " + nomUtilisateur + "!")
+score = score + 10
+print(score)
+//nomUtilisateur = "Leonard" //ceci est interdit car nomUtilisateur est une constante déclarée avec let
