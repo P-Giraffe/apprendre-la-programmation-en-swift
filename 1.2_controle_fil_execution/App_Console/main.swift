397bc19eefe4947143c2faf91ef3e7734a28c112
Maxime Britto <maxime@purplegiraffe.fr>
2020-05-27 23:09:29 +0200

    13_3_switch_case_en_Swift

---
+++ 1.2_controle_fil_execution/App_Console/main.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  AppConsole
+//
+//  Created by Maxime Britto on 02/05/2017.
+//  Copyright © 2017 mbritto. All rights reserved.
+//
+
+import Foundation
+
+print("Entrez votre age : ")
+let age:Int
+age = Utilisateur.saisirEntier()
+print("Age : \(age)")
+
+if age < 12 {
+    print("Vous êtes un enfant")
+} else if age < 18 {
+    print("Vous êtes un adolescent")
+} else {
+    print("Vous êtes un adulte")
+}
+
+switch age {
+case 1...12:
+    print("Vous êtes un enfant")
+//    fallthrough
+case 13..<18:
+    print("Vous êtes un adolescent")
+case 18:
+    print("Bravo pour votre majorité")
+default:
+    print("Vous êtes un adulte")
+}
+
