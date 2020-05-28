26c1aee8683b2d10a4ede70939def57a93156c45
Maxime Britto <maxime@purplegiraffe.fr>
2020-05-28 16:42:45 +0200

    13_6_La_boucle_for_en_Swift

---
+++ 1.3_boucles/App_Console/main.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  AppConsole
+//
+//  Created by Maxime Britto on 02/05/2017.
+//  Copyright © 2017 mbritto. All rights reserved.
+//
+
+import Foundation
+
+//Exemple de génération de nombre aléatoire
+let nombreAleatoire = Math.nombreAleatoire(comprisEntre: 1, et: 100)
+print(nombreAleatoire)
+for numeroTour in 0..<nombreAleatoire {
+    print("Coup de pelle numéro \(numeroTour)")
+}
+
+
+//Exemples de saisies utilisateur
+//print("Entrez votre prénom : ")
+//let texte:String
+//texte = Utilisateur.saisirTexte()
+//print("Bonjour \(texte)")
+//
+//print("Entrez votre age : ")
+//let age:Int
+//age = Utilisateur.saisirEntier()
+//print("Age : \(age)")
+//
+//print("Entrez une note : ")
+//let note:Double
+//note = Utilisateur.saisirReel()
+//print("Note : \(note)")
+//
+
+
